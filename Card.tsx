13c376c71a50d0283a7b07eaad9b62773268eaa8
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-06-25 11:25:03 -0400

    Simplify logic - fix issues

--- Card.tsx
+++ Card.tsx
@@ -6,24 +6,16 @@ function Card()
 {
     // Track card and mouse position in state
     const [cardPos, setCardPos] = useState<{top: number, left: number}>({top: 0, left: 0});
-    const [mousePos, setMousePos] = useState<{mouseX: number, mouseY: number}>({mouseX: 0, mouseY: 0});
 
     // Grab the card with the cursor
-    const grabCard = (e: any) => {
+    const grabCard = (e: React.MouseEvent) => {
         e.preventDefault();
 
-        console.log("Grabbed");
-
-        // Get cursor starting position
-        setMousePos({mouseX: e.screenX, mouseY: e.screenY});
-
         // Create an event handler to call dragCard when the mouse moves
-        document.onmousemove = (e: MouseEvent) => {dragCard(e)};
+        document.onmousemove = (move: MouseEvent) => {dragCard(move)};
 
         // Create an event handler to free the move event handler when the mouse button is released
         document.onmouseup = () => {
-            console.log("Released");
-
             document.onmousemove = null;
             document.onmouseup = null;
         }
@@ -33,15 +25,12 @@ function Card()
     const dragCard = (e: MouseEvent) => {
         e.preventDefault();
 
-        console.log("Moved");
-
-        // Move the card to the mouse and get the next mouse position
-        setCardPos({...cardPos, top: mousePos.mouseY, left: mousePos.mouseX});
-        setMousePos({...mousePos, mouseX: e.screenX, mouseY: e.screenY});
+        // Move the card to the mouse
+        setCardPos({...cardPos, top: e.clientY, left: e.clientX});
     }
 
     return(
-        <div id="test" className="card" onMouseDown={(e: any) => {grabCard(e)}} style={{...cardPos}}></div>
+        <div className="card" onMouseDown={(click: any) => {grabCard(click)}} style={{...cardPos}}></div>
     );
 }
 
