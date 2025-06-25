6f01f0f54105ee99df43b768607c62e30cec93f2
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-06-25 12:09:19 -0400

    Implement user-definable dimensions, use same system for default size to make CSS consistent

--- Card.tsx
+++ Card.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { type PropsWithChildren } from 'react';
 
 import './styles/Card.css';
@@ -6,7 +6,7 @@ import './styles/Card.css';
 type CardDimensions = {
     width: number,
     height: number,
-    units: string
+    units: "mm" | "px" | "rem" | "%"
 }
 
 interface props_Card {
@@ -14,20 +14,40 @@ interface props_Card {
     dimensions?: CardDimensions
 }
 
-function Card({img = undefined, dimensions = undefined, children = undefined}: PropsWithChildren<props_Card>)
+function Card({img = undefined, dimensions = {width: 63, height: 88, units: "mm"}, children = undefined}: PropsWithChildren<props_Card>)
 {
-    // Track card position in state
-    const [cardPos, setCardPos] = useState<{top: number, left: number}>({top: 0, left: 0});
+    type CardStyle = {
+        top: number,
+        left: number,
+        width?: string,
+        height?: string
+    }
+
+    // Object which holds all the data for the style attribute of this element
+    const [cardStyle, setCardStyle] = useState<CardStyle>({top: 0, left: 0});
+
+    // Variables for tracking which part of the card the user grabbed
     let offsetX: number = 0;
     let offsetY: number = 0;
+
+    // Add user-defined styles to cardStyle
+    useEffect(() => {
+        if (dimensions)
+        {
+            setCardStyle({...cardStyle, 
+                width: dimensions.width.toString() + dimensions.units,
+                height: dimensions.height.toString() + dimensions.units
+            })
+        }
+    }, [])
 
     // Grab the card with the cursor
     const grabCard = (e: React.MouseEvent) => {
         e.preventDefault();
 
         // Track the part of the card clicked
-        offsetX = e.clientX - cardPos.left;
-        offsetY = e.clientY - cardPos.top;
+        offsetX = e.clientX - cardStyle.left;
+        offsetY = e.clientY - cardStyle.top;
 
         // Create an event handler to call dragCard when the mouse moves
         document.onmousemove = (move: MouseEvent) => {dragCard(move)};
@@ -44,11 +64,11 @@ function Card({img = undefined, dimensions = undefined, children = undefined}: P
         e.preventDefault();
 
         // Move the card to the mouse
-        setCardPos({...cardPos, top: e.clientY - offsetY, left: e.clientX - offsetX});
+        setCardStyle({...cardStyle, top: e.clientY - offsetY, left: e.clientX - offsetX})
     }
 
     return(
-        <div className="card" onMouseDown={(click: any) => {grabCard(click)}} style={{...cardPos}}>
+        <div className="card" onMouseDown={(click: any) => {grabCard(click)}} style={{...cardStyle}}>
             {img ? <img src={img} className="card_img"/> : ""}
             {children ? children : ""}
         </div>
